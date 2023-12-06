56868eb8ca8b3256ed17694ed30246cb4eb03f87
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-12-05 21:52:26 -0300

    solucion problema de publicidad

---
+++ cypress/integration/formularios/formularios.spec.js
@@ -0,0 +1,23 @@
+context('Formularios',()=>{
+    //primero se define la url, en este caso toma la baseUrl+ cy.visit.
+    beforeEach(()=>{
+        cy.viewport(1280, 720) // Ancho: 1280px, Alto: 720px
+        cy.visit('/');
+    })
+
+  
+    it.only('Test #1 : Envio de datos vacios ',()=>{
+       //cy.visit('https://qavalidation.com/demo-form') 
+    })
+
+    it('Test #2 : Tipo de dato',()=>{});
+    it('Test #3 : Envio de datos requeridos',()=>{});
+    it('Test #4 : ',()=>{});
+    it('Test #5 : ',()=>{});
+    it('Test #6 : ',()=>{});
+    it('Test #7 : ',()=>{});
+    it('Test #8 : ',()=>{});
+    it('Test #9 : ',()=>{});
+    it('Test #10 : ',()=>{});
+    
+})
